335965aa7fd010b5dd422eb4a8af386b5c3ef567
eunjin <jej961019@gmail.com>
2020-07-13 16:07:52 +0900

    chat

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // 노드모듈안에 있어서 경로를 안 넣어줌
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.set('views', `${__dirname}/public`);
 app.set('view engine', 'html');
@@ -9,6 +12,15 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static('public', {
     extensions: ['html']
 }))
+
+io.on('connection', (socket) => {
+    console.log('User connected...');
+    socket.on('chatting', (data) => {
+        io.sockets.emit('chatting', data);
+    })
+})
+
+
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -20,6 +32,7 @@ app.get('/api/users', (req, res) => {
     }]
     res.send(users)
 })
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`server is running on ${PORT}`);
+// })
+http.listen(PORT, () => console.log(`server is running on ${PORT}`));
